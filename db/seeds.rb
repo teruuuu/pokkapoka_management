50741d183a486b9770d99b1eb3b135141e489915
teru <titlt.aiw6@gmail.com>
2022-01-10 14:00:24 +0900

    初期データの投入

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ActiveRecord::Base.connection.execute("TRUNCATE TABLE users RESTART IDENTITY CASCADE")
+
+user1 = User.create!(email: "test@example.com", password: "password", name: "サンプル太郎")
+
+puts "データの投入に成功しました！"
