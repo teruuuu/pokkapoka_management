2ad5959d5254f29e747875632f4fb88df70d46ca
teru <titlt.aiw6@gmail.com>
2022-01-13 16:06:40 +0900

    初期データの追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,10 @@
 
 ActiveRecord::Base.connection.execute("TRUNCATE TABLE users RESTART IDENTITY CASCADE")
 
-user1 = User.create!(email: "test@example.com", password: "password", name: "サンプル太郎")
+user1 = User.create!(email: "test1@example.com", password: "password1", name: "佐藤幸司")
+user2 = User.create!(email: "test2@example.com", password: "password2", name: "鈴木正人")
+user3 = User.create!(email: "test3@example.com", password: "password3", name: "高橋由希子")
+user4 = User.create!(email: "test4@example.com", password: "password4", name: "田中昇")
+user5 = User.create!(email: "test5@example.com", password: "password5", name: "伊藤亜希")
 
 puts "データの投入に成功しました！"
