6c52fecb89d1b81228eca4ab3dd0f0e64bc31030
teru <titlt.aiw6@gmail.com>
2022-01-14 16:59:56 +0900

    patient新規登録機能の実装

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -3,9 +3,14 @@ class PatientsController < ApplicationController
     @patients = Patient.includes(:user).order(:created_at)
   end
 
-  def new; end
+  def new
+    @patient = Patient.new
+  end
 
-  def create; end
+  def create
+    patient = current_user.patients.create!(patient_params)
+    redirect_to patient
+  end
 
   def show; end
 
@@ -14,4 +19,10 @@ class PatientsController < ApplicationController
   def update; end
 
   def destroy; end
+
+  private
+
+  def patient_params
+    params.require(:patient).permit(:name, :birthday, :gender, :address, :degree_of_care, :key_person)
+  end
 end
