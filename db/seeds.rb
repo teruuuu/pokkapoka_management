ce6dafd8f0aaed6f68c345529ee4dbae3560b1a8
teru <titlt.aiw6@gmail.com>
2022-01-20 22:00:02 +0900

    seedファイル追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,6 +19,9 @@ patient1 = user1.patients.create!(name: "佐藤一郎", birthday: "1930-04-01", 
 patient2 = user2.patients.create!(name: "鈴木幸子", birthday: "1956-01-15", gender: "女", address: "石岡市", degree_of_care: "要介護3", key_person: "夫")
 patient3 = user3.patients.create!(name: "高橋次郎", birthday: "1940-10-03", gender: "男", address: "那珂市", degree_of_care: "要介護1", key_person: "長女")
 
-information1 = user1.informations.create!(comment: "予後良好です", patient_id: patient1.id)
+information = user1.informations.create!(comment: "予後良好です。", patient_id: patient1.id)
+information = user1.informations.create!(comment: "玄関に段差があります。\nスロープ導入を紹介しています。", patient_id: patient1.id)
+information = user2.informations.create!(comment: "簡易入浴でお願いします。", patient_id: patient2.id)
+information = user3.informations.create!(comment: "食事は17時に徹底して下さい。", patient_id: patient3.id)
 
 puts "データの投入に成功しました！"
